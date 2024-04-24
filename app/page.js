68aeddaa39e7290d3b89e1ad7f523e7350c2a8d6
Liviadbelao <125027552+Liviadbelao@users.noreply.github.com>
2024-04-23 21:23:45 -0300

    tirei footer

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 import { Header } from './components/Header/Header'
 import styles from './page.module.css'
-import { Footer } from './components/Footer/Footer'
+
 
 export default function Home() {
   return (
@@ -12,7 +12,7 @@ export default function Home() {
           <h1 className={styles.h1}>Faça seus cálculos aqui </h1>
           
           </div>
-          <Footer />
+          
     </div>
   )
 }
